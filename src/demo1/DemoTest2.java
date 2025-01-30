ac1bea93ff7e9731bbfc0e029a67227624331de7
elkha <elkha@10.0.0.142>
2025-01-29 18:11:33 -0800

    Looping through b

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -3,5 +3,9 @@ package demo1;
 public class DemoTest2 {
 public static void main(String[] args) {
 	System.out.println("Java is Fun");
+	int b= 300;
+	for (int i = 0;i<b;i++) {
+		System.out.println(i);
+	}
 }
 }
